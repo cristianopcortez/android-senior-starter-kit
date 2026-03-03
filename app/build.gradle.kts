931e5e363c3da4f1d623779f795f43f39f8d1a9d
Cristiano Cortez <cristianocortez@gmail.com>
2026-03-03 12:38:25 -0300

    fix: add java.util.Properties import in app build script
    Fixes "Unresolved reference: util" when reading API_BASE_URL from local.properties.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -18,7 +20,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        val localProperties = java.util.Properties()
+        val localProperties = Properties()
         val localPropertiesFile = rootProject.file("local.properties")
         if (localPropertiesFile.exists()) {
             localProperties.load(localPropertiesFile.inputStream())
